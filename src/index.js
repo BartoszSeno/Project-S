55c39fe08883b319e8ec80aecae0cad5d234887c
Bartosz <nasieniu@gmail.com>
2022-08-29 19:19:54 +0200

    100% page scale from 100% to 75%

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import Register from "./routes";
 import "./media/mediahome.css";
+import "./media/medianews.css";
+import "./media/mediashop.css";
+import "./media/medialoginregister.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
