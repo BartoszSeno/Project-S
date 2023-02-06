acdb9627f8c0c1641613295d6557fb698bb50163
Bartosz <nasieniu@gmail.com>
2023-02-06 15:35:12 +0100

    fix nav size link click

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -5,15 +5,15 @@ function Header() {
   return (
     <div className="full_header">
       <div className="header">
-        <div className="home">
-          <Link to="/">HOME</Link>
-        </div>
-        <div className="news">
-          <Link to="/News">NEWS</Link>
-        </div>
-        <div className="shop">
-          <Link to="/shop">SHOP</Link>
-        </div>
+        <Link to="/">
+          <div className="home">HOME</div>
+        </Link>
+        <Link to="/News" className="nav-max">
+          <div className="news">NEWS</div>
+        </Link>
+        <Link to="/shop" className="nav-max">
+          <div className="shop">SHOP</div>
+        </Link>
       </div>
       <div className="header2">
         <Link to="/login">
